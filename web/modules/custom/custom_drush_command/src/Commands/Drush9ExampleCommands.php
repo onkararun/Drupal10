c50aa8626b56e8b786fa029938a1d2be21cc7fb3
onkararun <aryamaaru@gmail.com>
2023-02-13 16:40:21 +0530

    added two custom module

---
+++ web/modules/custom/custom_drush_command/src/Commands/Drush9ExampleCommands.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\custom_drush_command\Commands;
+
+use Drush\Commands\DrushCommands;
+
+/**
+ * A Drush commandfile.
+ *
+ * In addition to this file, you need a drush.services.yml
+ * in root of your module, and a composer.json file that provides the name
+ * of the services file to use.
+ */
+class Drush9ExampleCommands extends DrushCommands {
+  /**
+   * Echos back hello with the argument provided.
+   *
+   * @param string $name
+   *   Argument provided to the drush command.
+   *
+   * @command drush9_example:hello
+   * @aliases d9-hello
+   * @options arr An option that takes multiple values.
+   * @options msg Whether or not an extra message should be displayed to the user.
+   * @usage drush9_example:hello akanksha --msg
+   *   Display 'Hello Akanksha!' and a message.
+   */
+  public function hello($name, $options = ['msg' => FALSE]) {
+    if ($options['msg']) {
+      $this->output()->writeln('Hello ' . $name . '! This is your first Drush 9 command.');
+    }
+    else {
+      $this->output()->writeln('Hello ' . $name . '!');
+    }
+  }
+
+}
