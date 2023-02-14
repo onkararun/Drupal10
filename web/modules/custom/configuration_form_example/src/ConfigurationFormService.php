1f5cf90ae12faefd4c1463588ab420d5db2b63f4
onkararun <aryamaaru@gmail.com>
2023-02-14 14:16:32 +0530

    add dependency injection in config module

--- web/modules/custom/configuration_form_example/src/ConfigurationFormService.php
+++ web/modules/custom/configuration_form_example/src/ConfigurationFormService.php
@@ -6,17 +6,20 @@
  */
 
 namespace Drupal\configuration_form_example;
+use Drupal\Core\Config\ConfigFactoryInterface;
 
 class ConfigurationFormService {
   
   protected $configuration_value;
-  
-  public function __construct() {
-    $this->configuration_value = \Drupal::config('configuration_form_example.settings');
+  protected $configFactory;
+
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+    $this->configuration_value = $this->configFactory->getEditable('configuration_form_example.settings');
   }
   
   public function getValue() {
-    return $this->configuration_value;
+    return $this->configuration_value; 
   }
   
 }
