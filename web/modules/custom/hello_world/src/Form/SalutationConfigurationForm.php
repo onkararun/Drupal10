022a096b322f9458b3b06153c68686ef4e9d9405
onkararun <aryamaaru@gmail.com>
2023-03-08 21:46:07 +0530

    add new code in hello world module

--- web/modules/custom/hello_world/src/Form/SalutationConfigurationForm.php
+++ web/modules/custom/hello_world/src/Form/SalutationConfigurationForm.php
@@ -4,44 +4,81 @@ namespace Drupal\hello_world\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Logger\LoggerChannelInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Configuration form definition for the salutation message.
  */
 class SalutationConfigurationForm extends ConfigFormBase {
-    /**
-     * {@inheritdoc}
-     */
-    protected function getEditableConfigNames() {
-        return ['hello_world.custom_salutation'];
-    }
+  
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['hello_world.custom_salutation'];
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getFormId() {
-        return 'salutation_configuration_form';
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'salutation_configuration_form';
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(array $form, FormStateInterface $form_state) {
-        $config = $this->config('hello_world.custom_salutation');
-        $form['salutation'] = array(
-            '#type' => 'textfield',
-            '#title' => $this->t('Salutation'),
-            '#description' => $this->t('Please provide the salutation you want to use.'),
-            '#default_value' => $config->get('salutation'),
-        );
-        return parent::buildForm($form, $form_state);
-    }
+  /**
+  * @var \Drupal\Core\Logger\LoggerChannelInterface
+  */
+  protected $logger;
 
-    /**
-     * {@inheritdoc}
-     */
-    public function submitForm(array &$form, FormStateInterface $form_state) {
-        $this->config('hello_world.custom_salutation')->set('salutation', $form_state->getValue('salutation'))->save();
-        parent::submitForm($form, $form_state);
-    }
+  /**
+  * SalutationConfigurationForm constructor.
+  *
+  * @param \Drupal\Core\Config\ConfigFactoryInterface $config_
+  factory
+  *
+  The factory for configuration objects.
+  * @param \Drupal\Core\Logger\LoggerChannelInterface $logger
+  *
+  The logger.
+  */
+  public function __construct(ConfigFactoryInterface $config_factory, LoggerChannelInterface $logger) {
+    parent::__construct($config_factory);
+    $this->logger = $logger;
+  }
+
+  /**
+  * {@inheritdoc}
+  */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('hello_world.logger.channel.hello_world')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('hello_world.custom_salutation');
+    $form['salutation'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Salutation'),
+      '#description' => $this->t('Please provide the salutation you want to use.'),
+      '#default_value' => $config->get('salutation'),
+    );
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state){
+    $this->config('hello_world.custom_salutation')->set('salutation', $form_state->getValue('salutation'))->save();
+    parent::submitForm($form, $form_state);
+
+    $this->logger->info('The Hello World salutation has been changed to @message.', ['@message' => $form_state->getValue('salutation')]);
+  }
 }
