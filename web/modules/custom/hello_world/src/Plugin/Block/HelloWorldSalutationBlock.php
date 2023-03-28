651003fa7b7c4833e4930c23a5b81ad71fb747d8
onkararun <aryamaaru@gmail.com>
2023-03-28 22:17:19 +0530

    indentation

--- web/modules/custom/hello_world/src/Plugin/Block/HelloWorldSalutationBlock.php
+++ web/modules/custom/hello_world/src/Plugin/Block/HelloWorldSalutationBlock.php
@@ -16,75 +16,71 @@ use Drupal\Core\Form\FormStateInterface;
  * admin_label = @Translation("Hello world salutation"),
  * )
  */
-class HelloWorldSalutationBlock extends BlockBase implements ContainerFactoryPluginInterface
-{
-    /**
-     * The salutation service.
-     *
-     * @var \Drupal\hello_world\HelloWorldSalutation
-     */
-    protected $salutation;
+class HelloWorldSalutationBlock extends BlockBase implements ContainerFactoryPluginInterface {
 
-    /**
-     * Constructs a HelloWorldSalutationBlock.
-     */
-    public function __construct(array $configuration, $plugin_id, $plugin_definition, HelloWorldSalutation $salutation)
-    {
-        parent::__construct($configuration, $plugin_id, $plugin_definition);
-        $this->salutation = $salutation;
-    }
+  /**
+   * The salutation service.
+   *
+   * @var \Drupal\hello_world\HelloWorldSalutation
+   */
+  protected $salutation;
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
-    {
-        return new static(
-            $configuration,
-            $plugin_id,
-            $plugin_definition,
-            $container->get('hello_world.salutation')
-        );
-    }
+  /**
+   * Constructs a HelloWorldSalutationBlock.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, HelloWorldSalutation $salutation) {
+      parent::__construct($configuration, $plugin_id, $plugin_definition);
+      $this->salutation = $salutation;
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function defaultConfiguration()
-    {
-        return [
-            'enabled' => 1,
-        ];
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+      return new static(
+          $configuration,
+          $plugin_id,
+          $plugin_definition,
+          $container->get('hello_world.salutation')
+      );
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function build()
-    {
-        return [
-            '#markup' => $this->salutation->getSalutation(),
-        ];
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'enabled' => 1,
+    ];
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function blockForm($form, FormStateInterface $form_state) {
-        $config = $this->getConfiguration();
-        $form['enabled'] = array(
-            '#type' => 'checkbox',
-            '#title' => $this->t('Enabled'),
-            '#description' => $this->t('Check this box if you want to enable this feature.'),
-            '#default_value' => $config['enabled'],
-        );
-        return $form;
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+      return [
+        '#markup' => $this->salutation->getSalutation(),
+      ];
+  }
 
-    /**
-    * {@inheritdoc}
-    */
-    public function blockSubmit($form, FormStateInterface $form_state) {
-        $this->configuration['enabled'] = $form_state->getValue('enabled');
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $config = $this->getConfiguration();
+    $form['enabled'] = array(
+        '#type' => 'checkbox',
+        '#title' => $this->t('Enabled'),
+        '#description' => $this->t('Check this box if you want to enable this feature.'),
+        '#default_value' => $config['enabled'],
+    );
+    return $form;
+  }
+
+  /**
+  * {@inheritdoc}
+  */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['enabled'] = $form_state->getValue('enabled');
+  }
 }
